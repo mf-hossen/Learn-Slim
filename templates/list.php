1363473601d462c5534031cba6ce5bbde5c92dba
shanta <mimetyshanta@gmail.com>
2016-08-18 18:11:47 +0600

    update employee list

--- templates/list.php
+++ templates/list.php
@@ -8,7 +8,8 @@
     <title>Bootstrap 101 Template</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -18,60 +19,117 @@
     <![endif]-->
     <style>
 
-        th{
-            background-color: grey;
+        th {
+            background-color: black;
+            color: white;
+            text-align: right;
         }
 
-        caption{
+
+
+        caption {
 
             background-color: beige;
         }
     </style>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <table style="margin: 20px auto; background-color: aquamarine">
-                <caption>Employee Table</caption>
-                <tr><th>Id</th><th>Name</th><th>Designation</th><th>Salary</th></tr>
+
+<div class="container">
+
+    <ol class="breadcrumb">
+             
+        <li><a href="#">Home</a></li>
+               
+        <li><a href="#">Employee</a></li>
+    </ol>
+           
+    <div class="row">
+               
+        <div class="col-md-6">
+                           <h2 class="pull-left">Employee List</h2>
+                   
+        </div>
+               
+        <div class="col-md-6">
+                       <a href="#" class=" glyphicon glyphicon-plus btn btn-info pull-right" type="button">
+                CREATE</a>
+
+
+                   
+        </div>
+    </div>
+
+    <div class="row">
+        <table class="table table-responsive">
+            <tr>
+
+                <th>Name</th>
+                <th>Designation</th>
+
+                <th>Department</th>
+                <th>Department</th>
+                <th>Salary</th>
+                <th>Action</th>
+
+            </tr>
             <div class="col-md-6" style="text-align: center">
                 <?php
-                foreach ($employee as $emp){
-                    echo "<tr>";
-                    echo "<td>";
-                    echo $emp['id'];
-                    echo "</td>";
 
-                    echo "<td>";
+
+                // english notation (default)
+                foreach ($employee as $emp) {
+                    echo "<tr>";
+                    //echo "<td>";
+                    //echo $emp['id'];
+                    //echo "</td>";
+
+                    echo "<td class='text-right'>";
                     echo $emp['name'];
                     echo "</td>";
 
-                    echo "<td>";
+                    echo "<td class='text-right'>";
                     echo $emp['designation'];
                     echo "</td>";
 
-                    echo "<td>";
-                    echo $emp['salary'];
+
+
+                    echo "<td class='text-right'>";
+                    echo $emp['department'];
                     echo "</td>";
+
+                    echo "<td class='text-right'>";
+                    echo $emp['work_time'];
+                    echo "</td>";
+
+                    echo "<td class='text-right'>";
+                    echo $number= number_format($emp['salary']);;
+                    echo "</td>";
+
+                    echo "<td class='text-right'>";
+                    echo "<span class='glyphicon glyphicon-eye-open' title=\"View Employee\" aria-hidden=\"true\"></span> ";
+                    echo "<span class='glyphicon glyphicon-pencil' title=\"Edit Employee\" aria-hidden=\"true\" style='color: green'></span>  ";
+                    echo "<span class='glyphicon glyphicon-remove' title=\"Remove Employee\" aria-hidden=\"true\" style='color: red'></span> ";
+                    echo "</td>";
+
                     echo "</tr>";
                 }
                 ?>
 
             </div>
-            </table>
-            <div class="col-md-6">
+        </table>
+        <div class="col-md-6">
 
 
-            </div>
         </div>
+    </div>
 
 
-
-
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
 </body>
 </html>
 
