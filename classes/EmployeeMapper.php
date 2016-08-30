5ac097b893fa6c64a447382dbc8b92448a785968
shanta <mimetyshanta@gmail.com>
2016-08-30 16:47:29 +0600

    attendence

--- classes/EmployeeMapper.php
+++ classes/EmployeeMapper.php
@@ -24,13 +24,13 @@ on employe.department_id = department.id  and employe.designation_id = designati
         {
             // var_dump($data); die();
             try {
-                $stmt = $this->db->prepare("INSERT INTO employe (name,designation_id,department_id,work_time,salary)VALUES (:name,:designation_id,:department_id,:work_time,:salary)");
+                $stmt = $this->db->prepare("INSERT INTO employe (name,designation_id,department_id,work_time,salary,description)VALUES (:name,:designation_id,:department_id,:work_time,:salary,:description)");
                 $stmt->bindParam(':name', $data['name']);
                 $stmt->bindParam(':designation_id', $data['designation_id']);
                 $stmt->bindParam(':department_id', $data['department_id']);
                 $stmt->bindParam(':work_time', $data['work_time']);
                 $stmt->bindParam(':salary', $data['salary']);
-
+                $stmt->bindParam(':description', $data['description']);
                 $stmt->execute();
 
                 return $this->db->lastInsertId();
@@ -45,7 +45,7 @@ on employe.department_id = department.id  and employe.designation_id = designati
 
     public function GetbyId($id)
     {
-        $sql = "SELECT employe.id ,employe.name, employe.work_time , employe.salary, department.name as department_name , designation.name as designation_name
+        $sql = "SELECT employe.id ,employe.name, employe.work_time ,employe.description, employe.salary, department.name as department_name , designation.name as designation_name
 FROM `employe` 
 join department join designation 
 on employe.department_id = department.id  and employe.designation_id = designation.id where employe.id='$id'";
@@ -81,7 +81,7 @@ on employe.department_id = department.id  and employe.designation_id = designati
     public function GetDetails($id)
     {
 
-        $sql = "SELECT employe.id as emplye_id,employe.name, employe.work_time , employe.salary,designation.id as desi_id,department.id as desi_id, department.name as department_name , designation.name as designation_name
+        $sql = "SELECT employe.id as emplye_id,employe.name, employe.work_time , employe.salary,employe.description, designation.id as desi_id,department.id as desi_id, department.name as department_name , designation.name as designation_name
 FROM `employe` 
 join department join designation 
 on employe.department_id = department.id  and employe.designation_id = designation.id 
@@ -100,6 +100,66 @@ where employe.id='$id'";
         $stmt = $this->db->query($sql);
         return $stmt;
     }
+
+
+    public function empAttendence($data)
+    {
+        {
+            //var_dump($data); die();
+            try {
+
+                $stmt = $this->db->prepare("INSERT INTO attendence (employee_id,check_in,check_out,date,year)
+VALUES (:employee_id,:check_in,:check_out,:date,:year)");
+                $stmt->bindParam(':employee_id', $data['employee_id']);
+                $stmt->bindParam(':check_in', date('Y-m-d h:i:s'));
+                $stmt->bindValue(':check_out', null);
+                $stmt->bindParam(':date', date('Y-m-d'));
+                $stmt->bindParam(':year', date('Y'));
+                //$stmt->bindParam(':status', $data['Present']);
+                $stmt->execute();
+
+                return $this->db->lastInsertId();
+
+            } catch (Exception $e) {
+                throw $e;
+
+            }
+        }
+    }
+
+
+    public function getAttendence($date)
+    {
+	$date=date('Y-m-d');
+        $sql ="select * from attendence where date BETWEEN '$date' and '$date'";
+        $stmt = $this->db->query($sql);
+        $result = [];
+
+        while ($row = $stmt->fetchAll()) {
+
+            $result = $row;
+        }
+        return $result;
+    }
+
+    public function empPunch($data)
+    {
+        try {
+
+            $id = $data['employee_id'];
+            var_dump($id);die();
+            $stmt = $this->db->prepare("update attendence set check_out=:check_out where employee_id='$id'");
+            $stmt->bindParam(':check_out', date('Y-m-d h:i:s'));
+            $stmt->execute();
+            $dd = $this->getAttendence($id);
+            return $dd['employee_id'];
+        } catch (Exception $e) {
+            throw $e;
+
+        }
+
+    }
+
 
 
 }
